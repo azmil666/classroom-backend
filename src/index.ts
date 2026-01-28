edf32133475c6915862b13b23ba6f4caad0ee64a
Azmil Mohammed <azmilmohammed@Azmils-MacBook-Air.local>
2026-01-28 18:18:54 +0530

    added more pages and login/sign-up

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,9 @@ import cors from 'cors';
 import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
 import classesRouter from "./routes/classes.js";
+import departmentsRouter from "./routes/departments.js";
+import statsRouter from "./routes/stats.js";
+import enrollmentsRouter from "./routes/enrollments.js";
 import securityMiddleware from "./middleware/security.js";
 import {toNodeHandler} from "better-auth/node";
 import {auth} from "./lib/auth.js";
@@ -38,9 +41,11 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(express.json());
+
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
-app.use(express.json());
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(securityMiddleware);
@@ -50,6 +55,9 @@ if (process.env.NODE_ENV === "production") {
 app.use('/api/subjects', subjectsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/classes', classesRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/stats", statsRouter);
+app.use("/api/enrollments", enrollmentsRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
